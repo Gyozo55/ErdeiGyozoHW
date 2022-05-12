b077330daf1573066e440adaa28061a5e04ddc33
Gyozo55 <erdeigyo@gmail.com>
2022-05-12 21:07:19 +0200

    [#1] feat: Create all methods in databaseHandler app

--- backend/databaseHandler/views.py
+++ backend/databaseHandler/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from backend.databaseHandler.models import SpendingList
 
-# Create your views here.
+
+def get_all_spendings_from_db():
+    return SpendingList.objects.all().value()
+
+
+def create_new_spending(description, amount, spent_at, currency):
+    SpendingList(description=description, amount=amount, spent_at=spent_at,
+                 currency=currency).save()
+
+
+def order_data_by_spendings(order_type):
+    return SpendingList.objects.order_by(f'{order_type}')
+
+
+def filter_data_by_currency(filter_type):
+    return SpendingList.objects.filter(curreancy=filter_type).all().value()
